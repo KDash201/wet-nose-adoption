6ba9d745092acd2dc98baebc824137758d1d166d
KDash201 <KVonMadison@Yahoo.com>
2021-12-02 20:02:17 -0500

    Creation of Pet Finder Section

---
+++ assets/scripts/script.js
@@ -0,0 +1,13 @@
+// fetch("https://placedog.p.rapidapi.com/300/200", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "placedog.p.rapidapi.com",
+// 		"x-rapidapi-key": "c824df4ea9msh67c3deb5b02b662p1ab6dbjsnd8f3eec298a6"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
